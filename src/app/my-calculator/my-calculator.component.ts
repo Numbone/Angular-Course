c256c95bb927b36d8cd9b270393c085d60da00f9
Nurlybek <nurlybekqwerty@gmail.com>
2024-05-13 22:58:18 +0500

    ngFor and ng container  interface enum in angular try to rebuild component calculator

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,18 +1,63 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {KeyValuePipe, NgForOf} from "@angular/common";
+
+interface CalcGroup{
+  first:CalcVar,
+  second:CalcVar,
+  operation:CalcOperations
+}
+
+interface CalcVar{
+  value:number;
+  modification: CalcModifiers;
+}
+
+enum CalcOperations{
+  PLUS="+",
+  MINUS="-",
+  MULTIPLY="*",
+  DIVIDER="/"
+}
+
+enum CalcModifiers{
+  NONE="NONE",
+  SIN="SIN",
+  COS="COS",
+  SQUARE="SQUARE"
+}
 
 @Component({
   selector: 'app-my-calculator',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    KeyValuePipe
   ],
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.scss'
 })
 export class MyCalculatorComponent {
+  public calcOperations=CalcOperations;
+  public calcModifiers: CalcModifiers=CalcModifiers.NONE;
+  public calcGroup:CalcGroup[]=[
+    {
+      first:{
+        modification:CalcModifiers.NONE,
+        value:0
+      },
+      second:{
+        modification:CalcModifiers.NONE,
+        value:0
+      },
+      operation:CalcOperations.PLUS
+    }
+  ];
+  public history:string[]=[];
+
+  public operationBetweenGroups:CalcOperations[]=[]
+
   public first:number=1;
   public second:number=1;
 
